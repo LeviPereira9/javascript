767fcc0a4599945c81730ac5330ce4a8cf773bd0
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2022-05-19 17:13:42 -0300

    Repetições

---
+++ modulo-e/ambiente.js
@@ -0,0 +1,17 @@
+/* var c = 1
+while (c <= 6) {
+    console.log(`Olá, mundo ${c}.`)
+    c++
+} */
+
+
+/* var c = 1
+
+do {
+    console.log(`Olá, mundo ${c}.`)
+    c++
+} while (c <= 6) */
+
+for (var c=10; c>=1; c--) {
+    console.log(`Olá senhor ${c}`)
+}

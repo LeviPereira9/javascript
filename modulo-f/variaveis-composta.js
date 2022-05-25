6420067f68e5bdddbb811dd6d914da74cdd7e8e9
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2022-05-25 15:37:05 -0300

    array

---
+++ modulo-f/variaveis-composta.js
@@ -0,0 +1,8 @@
+/* let num = [5, 8, 2, 9, 3]
+
+num.push(7)
+num.sort() //Coloca em ordem crescente. :)
+
+console.log(num)
+console.log(`Nosso vetor têm ${num.length} posições`)
+console.log(`Nosso primeiro valor é ${num[0]}`) */
